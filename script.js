0777d87f6b0e2551bcc62163252420c59d160ce4
Erick Divin <56797651+erick1326a@users.noreply.github.com>
2026-09-05 17:05:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -212,12 +212,3 @@ difficultySelect.addEventListener('change', () => {
 });
 
 initGame();
-
-// Test Restart Button
-const restartBtn = document.getElementById('restart-test');
-if (restartBtn) {
-    restartBtn.addEventListener('click', () => {
-        localStorage.removeItem('rentdleState');
-        window.location.reload();
-    });
-}
